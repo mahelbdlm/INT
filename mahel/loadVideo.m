3ecb4062b9ac633c01687de927806d796b9a5aed
Mahel Berthod DLM <65778132+mahelbdlm@users.noreply.github.com>
2024-10-14 12:08:22 +0200

    Update loadVideo.m

--- mahel/loadVideo.m
+++ mahel/loadVideo.m
@@ -1,3 +1,16 @@
 % Import video file and work with it
-vid = load("video_color.mat")
-imshow(vid.video_color(100).df)
+%vid_color= load("video_color.mat");
+%vid_depth= load("video_depth.mat");
+
+imageArr=[];
+imgindex=1;
+numberImagesHorizontal = 4;
+for i=1:10:150
+    rotated_img = imrotate(vid_depth.video_depth(i).df, -90);
+    img_colormap = ind2rgb(gray2ind(mat2gray(rotated_img), 256), parula(256));
+    imageArr{imgindex} = vid_color.video_color(i).df;
+    imageArr{imgindex+1} = img_colormap;
+    imgindex=imgindex+2;
+end
+
+montage(imageArr,'Size', [round(imgindex/numberImagesHorizontal)-1, numberImagesHorizontal]);
