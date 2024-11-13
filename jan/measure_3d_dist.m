4f71d7224461746ba1df0d2f3f3e58cd7bae2afa
janfiguls <jan.figuls@estudiantat.upc.edu>
2024-11-13 19:15:38 +0100

    Removed function measure
    Renamed function measure to measure_3d_dist
    
    Made a copy of Mahel's code select_2_pixels and edited last part

--- jan/measure_3d_dist.m
+++ jan/measure_3d_dist.m
@@ -1,7 +1,7 @@
 function [dist12]=measure_3d_dist(P1,P2,I) 
 %MEASURE Measure the 3d distance between two pixels P1 and P2
 %
-% P1 and P2 are cells containing the {row=hpos,column=wpos} of the pixels we
+% P1 and P2 are 1x2 matrices containing the (row=hpos,column=wpos) of the pixels we
 %want to measure. 
 % 
 % I is the 2D depth_frame from where we get the depth value of the pixels we want
@@ -20,14 +20,14 @@ function [dist12]=measure_3d_dist(P1,P2,I)
     VFOV=62*pi/180;
 
 % Check that I is a valid 2D matrix
-    [height,width,check_2DMatrix]=size(I);
-    if or(isnumeric(I),check_2DMatrix==1)
+    [height,width,check_is2dMatrix]=size(I);
+    if ~and(isnumeric(I),check_is2dMatrix==1)
         error('Make shure that I is indeed a depth frame (2D matrix) and not a colorized_depth_frame (3D matrix)');
     end
     I=double(I);
 % Check that P1 and P2 are within the image
-    [wpos1,hpos1]=P1{:};
-    [wpos2,hpos2]=P2{:};
+    hpos1=P1(1);wpos1=P1(2);
+    hpos2=P2(1);wpos2=P2(2);
     try
         depth1=I(hpos1,wpos1);
         depth2=I(hpos2,wpos2);
@@ -44,6 +44,6 @@ function [dist12]=measure_3d_dist(P1,P2,I)
         w2=2*(wpos2-width/2)*(depth2*tan(HFOV/2))/width;
     end
     dist12x=(HFOV/VFOV)*(w1+w2);
-    dist12y=(HFOV/VFOV)*2*(-depth1*(hpos1-height/2)+depth2*(hpos2-height/2))*tan(HFOV/2)/height;
+    dist12y=(VFOV/HFOV)*2*(-depth1*(hpos1-height/2)+depth2*(hpos2-height/2))*tan(HFOV/2)/height;
     dist12=hypot(hypot(dist12x,dist12y),abs((depth1)-(depth2)))/1000;%in meters!
 end
