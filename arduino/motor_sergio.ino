a01a1c1567b4c8a58bcc66369e32854b714bf83c
serrines <serrinesp00@gmail.com>
2024-12-02 21:09:42 +0100

    Update motor_sergio.ino

--- arduino/motor_sergio.ino
+++ arduino/motor_sergio.ino
@@ -2,15 +2,18 @@
 //to turnOn (lock), turnOff and spin indefinitely until 0 is sent, respectively
 //Added code to implement a push-button to activate the motor and a potentiometer 100K to vary the rpm.
 //Also added potentiometer 1K to the 12V supply for control.
+//Added booting function to let the motor start slowly pulling the pallet until it reaches the wanted speed.
 #include <Stepper.h>
 
 int val = 0;
-int rpm = 200;
+int rpm = 300;
 const int stepsPerRevolution = 200;
 
 int buttonPin = 2;
 int buttonState = 0;
 int poteValue;
+
+int enable_arr = 0;
 
 
 Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
@@ -31,21 +34,36 @@ void turnOff() {
   digitalWrite(11, LOW);
 }
 
+void arranque() {
+  for (int i=150; i<rpm; i+=1) {
+      myStepper.setSpeed(i);
+      myStepper.step(10);
+  }
+}
+
 void setup() {
   Serial.begin(9600);
   pinMode(buttonPin, INPUT);
 }
 
 void loop() {
+  enable_arr = 1;
   buttonState = digitalRead(buttonPin);
   
   while (buttonState == HIGH) {
     poteValue= analogRead(A0); //goes from 0 to 1023
     rpm = map(poteValue, 0, 1023, 0, 300);
     Serial.print(rpm);
-    
+
+    //Arranque
+    if (enable_arr == 1) {
+      arranque();
+    }
+    enable_arr = 0;
+    //Velocidad constante
     myStepper.setSpeed(rpm);
     myStepper.step(stepsPerRevolution);
+    
     buttonState = digitalRead(buttonPin);
   }
   turnOn();
